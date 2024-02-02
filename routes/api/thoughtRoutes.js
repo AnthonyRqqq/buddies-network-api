3d947bfaa8e612fe96fc33db04f14c7ed0e7e23f
unknown <anthony_rosati@yahoo.com>
2024-02-01 21:51:25 -0600

    Added functionality to thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { Thought } = require('../../models');
 
 // The /api/thoughts route
 // GET route for all thoughts
 router.get('/', async (req,res) => {
     try {
+        const data = await Thought.find();
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -15,6 +19,11 @@ router.get('/', async (req,res) => {
 // GET route for a single thought by its _id
 router.get('/:_id', async (req, res) => {
     try {
+        const data = await Thought.findOne({
+            _id: req.params._id
+        });
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -26,7 +35,10 @@ router.get('/:_id', async (req, res) => {
 // POST route for creating a new thought
 router.post('/', async (req, res) => {
     try {
-         
+         const data = await Thought.create(req.body);
+
+         res.json(data);
+
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
@@ -37,6 +49,20 @@ router.post('/', async (req, res) => {
 // PUT route for updating a thought by its _id
 router.put('/:_id', async (req,res) => {
     try {
+        const data = await Thought.findOneAndUpdate(
+            {
+                _id: req.params._id
+            },
+            {
+                $set: req.body
+            },
+            {
+                runValidators: true,
+                new: true
+            }
+        );
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -48,7 +74,12 @@ router.put('/:_id', async (req,res) => {
 // DELETE route for deleting a thought by its _id
 router.delete('/:_id', async (req,res) => {
     try {
+        const data = await Thought.findOneAndDelete({
+            _id: req.params._id
+        });
 
+        res.json('Delete successful');
+        
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
