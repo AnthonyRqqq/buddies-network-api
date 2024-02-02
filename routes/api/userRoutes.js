5d7ce0e496db0ed3e2cb6b3396fa497c868c4d00
unknown <anthony_rosati@yahoo.com>
2024-02-01 21:44:51 -0600

    Added code to userRoutes.js for functionality

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { User } = require('../../models')
 
 // The /api/users route
 // GET route for all users
 router.get('/', async (req, res) => {
     try {
+        const data = await User.find();
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -15,6 +19,12 @@ router.get('/', async (req, res) => {
 // GET route for getting a single user by its _id
 router.get('/:_id', async (req, res) => {
     try {
+        const data = await User.findOne({ _id: req.params._id })
+        .populate([
+            'thoughts', 'friends'
+        ]);
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -26,7 +36,10 @@ router.get('/:_id', async (req, res) => {
 // POST route for creating a new user
 router.post('/', async (req, res) => {
     try {
-        
+        const data = await User.create(req.body);
+
+        res.json(data);
+
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
@@ -37,6 +50,20 @@ router.post('/', async (req, res) => {
 // PUT route for updating a user by its _id
 router.put('/:_id', async (req, res) => {
     try {
+        const data = await User.findOneAndUpdate(
+            {
+                _id: req.params._id
+            },
+            {
+                $set: req.body
+            },
+            {
+                runValidators: true,
+                new: true
+            }
+        );
+
+        res.json(data);
 
     } catch (err) {
         console.error(err);
@@ -48,7 +75,14 @@ router.put('/:_id', async (req, res) => {
 // DELETE route for deleting a user by its _id
 route.delete('/:_id', async (req, res) => {
     try {
+        const data = await User.findOneAndDelete(
+            {
+                _id: req.params._id
+            }
+        );
 
+        res.json('Delete successful');
+        
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
