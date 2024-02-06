33c0f01d5f93a284f434d5f6f14a7be92d28ae32
unknown <anthony_rosati@yahoo.com>
2024-02-05 19:14:52 -0600

    Added seed script to package.json, seed test successful

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { Thought, User } = requre('../models');
+const { Thought, User } = require('../models');
 const { userData, thoughtData } = require('./data')
 
 connection.on('error', (err) => err);
