39513c68e078aacd39b69931b215161d09402280
unknown <anthony_rosati@yahoo.com>
2024-02-05 19:12:46 -0600

    Added code to data.js

---
+++ utils/data.js
@@ -0,0 +1,31 @@
+const userData = [
+    {
+        username: 'Bob',
+        email: 'bob@bob.com'
+    },
+    {
+        username: 'Bill',
+        email: 'bill@bill.com'
+    },
+    {
+        username: 'Jill',
+        email: 'jill@jill.com'
+    }
+];
+
+const thoughtData = [
+    {
+        thoughtText: 'test thought 1',
+        username: 'Bob'
+    },
+    {
+        thoughtText: 'test thought 2',
+        username: 'Bob'
+    },
+    {
+        thoughtText: 'test thought 3',
+        username: 'Jill'
+    }
+];
+
+module.exports = { userData, thoughtData };
