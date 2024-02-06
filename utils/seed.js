d4a3e71a9c28f20998f9097154639c1fdfeddb24
unknown <anthony_rosati@yahoo.com>
2024-02-05 19:09:40 -0600

    Created seed and data files in utils folder, added code to seed.js

---
+++ utils/seed.js
@@ -0,0 +1,30 @@
+const connection = require('../config/connection');
+const { Thought, User } = requre('../models');
+const { userData, thoughtData } = require('./data')
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connection successful');
+
+    // Checks for preexisting user collection, deletes if exists
+    const userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+        await connection.dropCollection('users');
+    };
+
+    // Checks for preexisting thought collection, deletes if exists
+    const thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+    };
+
+    // Creates new collections with seeded data
+    await User.collection.insertMany(userData);
+    await Thought.collection.insertMany(thoughtData);
+
+    console.table(userData);
+    console.table(thoughtData);
+    console.info('Seeding complete');
+    process.exit(0);
+});
